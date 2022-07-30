7bdd6b146836111d98e7e26327e314cd1baedd2c
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-07-30 23:52:42 +0300

    Task 19 added

---
+++ Task19/Program.cs
@@ -0,0 +1,9 @@
+﻿string number = Console.ReadLine("Введите пятизначное число: ");
+if (number[1]== number[2] && number[2]==number[4])
+{
+    Console.WriteLine("Да");
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}

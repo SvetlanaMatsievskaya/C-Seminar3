50b8b342a5e5c21865db9b22aa1f0d18cc4e4231
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-07-31 02:07:25 +0300

    Task23 completed

---
+++ Task 23/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = 0;
+Console.WriteLine("Введите число: ");
+number = int.Parse(Console.ReadLine());
+int PowToThree(int number)
+{
+    int result = 0;
+    result = number*number*number;
+    return result;
+}
+for (int i =1; i < number+1; i++)
+{
+    Console.Write($"{PowToThree(i)} ");
+}

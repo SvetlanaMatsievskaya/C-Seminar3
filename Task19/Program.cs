87b9f19290356030966962e134391aa1e2ebd22f
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-07-31 11:48:28 +0300

    Task 19 updated

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,6 +1,10 @@
 ﻿Console.WriteLine("Введите пятизначное число: ");
 string number = Console.ReadLine();
-if (number[0]== number[4] && number[1]==number[3])
+if (number.Length >5 || number.Length < 5)
+{
+    Console.Write("Число не пятизначное");
+}
+else if (number[0]== number[4] && number[1]==number[3])
 {
     Console.WriteLine("Да");
 }    
